56b1b2188cb1db8134080e71584d7814847bf60a
Oliver Lindemann <oliver.lindemann@cognitive-psychology.eu>
2015-06-22 10:37:22 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# jasp-linux-install
-JASP installation script for Debian &amp; Ubuntu Linux
+Install JASP on Debian/Ubuntu 14.10+
+====================================
+
+To install [JASP 0.6.6](https://jasp-stats.org/) just download [this script](https://raw.githubusercontent.com/lindemann09/jasp-linux-install/master/install_jasp.sh)
+and execute it as sudo. Like this:
+
+```
+wget -P /tmp 
+https://raw.githubusercontent.com/lindemann09/jasp-linux-install/master/install_jasp.sh
+sudo sh /tmp/install_jasp.sh
+```
+
+
+
---
+++ install_jasp.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Run this bash script as sudo to install 
+# JASP on Debian or Ubuntu 14.10+ and Ubuntu 15.10
+#
+# see JASP website: https://jasp-stats.org/
+#
+# (c) 2015 Oliver Lindemann, MIT license
+
+VERSION="0.6.6"
+DISTRI="U1410"
+
+JASP="JASP-$VERSION-$DISTRI"
+SOURCEZIP="$JASP.zip"
+URL="https://static.jasp-stats.org/$SOURCEZIP"
+DEST="/opt"
+TMP="/tmp"
+
+# dependencies
+sudo apt-get install libboost-dbg libqt5webkit5 liblapack3 libblas-common 
+libarchive13 qt5-default # download
+
+#download zip
+echo "Downloading $SOURCEZIP"
+rm -f $TMP/$SOURCEZIP
+wget -P $TMP $URL
+
+# install
+cd $TMP
+unzip $TMP/$SOURCEZIP
+sudo rm -rf $DEST/$JASP
+sudo mv -f $JASP $DEST
+sudo ln -fs $DEST/$JASP/JASP /usr/bin/jasp-$VERSION
+
+# application menu
+sudo echo "[Desktop Entry]
+Exec=$DEST/$JASP/JASP %F
+Icon=None
+Type=Application
+Terminal=false
+Name="JASP $VERSION"
+Categories=Development;Science;" > /usr/share/applications/jasp-${VERSION}.desktop
